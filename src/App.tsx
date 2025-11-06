7090cbd37bd088e631e640f38559fa69ece261eb
yeonhee7935 <yeonhee7935@naver.com>
2025-11-06 09:25:13 +0900

    Refactor: 핀치줌 비활성화

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,19 @@ import { Routes, Route } from "react-router-dom";
 import SelectionPage from "./pages/SelectionPage";
 import LoadingPage from "./pages/LoadingPage";
 import ResultPage from "./pages/ResultPage";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(()=>{
+    const preventZoom = (e: TouchEvent)=>{
+      if(e.touches.length > 1) e.preventDefault()
+    }
+
+    document.addEventListener("touchmove", preventZoom, {passive: false})
+    return ()=>{
+      document.removeEventListener("touchmove", preventZoom)
+    }
+  },[])
   return (
     <Routes>
       <Route path="/" element={<SelectionPage />} />
